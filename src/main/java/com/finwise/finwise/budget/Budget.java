d264f91b2c99cfa720c2dd8052eb1a436f0fed30
ViniciuPSantos <vipaulasantos@gmail.com>
2026-05-29 20:17:01 -0300

    feat: add Budget entity

---
+++ src/main/java/com/finwise/finwise/budget/Budget.java
@@ -0,0 +1,41 @@
+package com.finwise.finwise.budget;
+
+import com.finwise.finwise.auth.User;
+import com.finwise.finwise.category.Category;
+import com.finwise.finwise.shared.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "budgets", uniqueConstraints = @UniqueConstraint(name = "uk_budget_user_category_period", columnNames = {
+        "user_id", "category_id", "budget_year", "budget_month" }))
+@Getter
+@Setter
+@NoArgsConstructor
+public class Budget extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, precision = 19, scale = 2)
+    private BigDecimal amount;
+
+    @Column(name = "budget_year", nullable = false)
+    private Integer year;
+
+    @Column(name = "budget_month", nullable = false)
+    private Integer month; 
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+}
